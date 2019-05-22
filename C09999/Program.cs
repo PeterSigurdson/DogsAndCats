c1dc77109363818b7237197371d87e99a06f36ee
Peter Sigurdson <peter@petersigurdson.net>
2019-05-22 11:39:51 -0400

    initial commit

---
+++ C09999/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C09999
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        } 
+    }
+
+    class Dog
+    {
+        static Random rand = new Random();
+        static int HowManyDogsHaveBeenCreated = 0;
+        string DogName;
+        public int SetStrength() {
+            return rand.Next(1,100);
+        }
+       
+    }
+
+    class Cat
+    {
+        static int HowManyCatsHaveBeenCreated = 0;
+        string CatName;
+        public int Strength;
+        public int SetStrength()
+        {
+            return rand.Next(1, 100);
+        }
+    }
+ }
